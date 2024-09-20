af4a05852a60dfcfee65bd2d83b56008625853a0
野鸽儿 <1634206675@qq.com>
2024-09-20 10:44:39 +0800

    xiaoshuo.py

--- xiaoshuo/xiaoshuo.py
+++ xiaoshuo/xiaoshuo.py
@@ -5,13 +5,13 @@ import requests
 from lxml import etree
 
 # 发送给谁
-url = 'https://dldl1.nsbuket.cc/xiaoshuo/douluodalu/1.html'   #起始网址
+url = 'https://dldl1.nsbuket.cc/xiaoshuo/douluodalu/1.html'   #起始网址，比如我这里用的斗罗大陆引子目录为起始地址
 
 while True:
     # 将代码访问伪装成游览器访问
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0'
-    }
+    }     # 用户代理可以通过游览器查找
 
     # 发送请求
     respond = requests.get(url,headers=headers)
@@ -25,11 +25,12 @@ while True:
     title = e.xpath('//h1/text()')[0]
     url = f'https://dldl1.nsbuket.cc{e.xpath("//tr/td[2]/a/@href")[0]}'   #下一章节地址
 
-    print(title)
+    print(title)  #将目录打印在终端方便查看下载进度
     # 保存
-    with open('斗罗大陆.txt','a',encoding='utf-8') as a:
+    with open('斗罗大陆.txt','a',encoding='utf-8') as a:  # 'a'表示追加数据，不能用'w'，会覆盖掉原来的数据
         a.write(title+'\n\n'+info+'\n\n')
 
+    # 下载到最后一章停止，退出循环，防止重头再下载一遍
     if(url == 'https://dldl1.nsbuket.cc/xiaoshuo/douluodalu/'):
         break 
    
